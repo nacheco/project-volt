1857060824b6d28f52061aa06189ee4228f52fed
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-01 16:23:13 -0500

    Completed Navbar.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navContainer}>
+        <div className={styles.imgContainer}>
+          <Link href="/" passHref>
+            <Image
+              className={styles.logo}
+              src="/images/LOGO.svg"
+              alt="logo"
+              width={50}
+              height={50}
+            />
+          </Link>
+        </div>
+        <ul className={styles.list}>
+          <li className={styles.list_item}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={styles.list_item}>
+            <Link href="https://www.natepacheco.dev/">Portfolio</Link>
+          </li>
+          <li className={styles.list_item}>
+            <Link href="https://www.natepacheco.dev/#contact">Contact</Link>
+          </li>
+        </ul>
+        <Link href="/" passHref>
+          <button className={styles.tryBtn}>Try Now</button>
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
