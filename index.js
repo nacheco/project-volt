8a07110e1befde428f240bed34d02aac0041f587
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-01 20:22:33 -0500

    Aquired dependencies, started server.

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
+
+io.on("connection", (socket) => {});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
