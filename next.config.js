2346f05f08a536a58189ebbb7ee52590fed391f1
Natanael Pacheco <natepacheco716@gmail.com>
2022-05-04 16:44:39 -0500

    Reverted Changes.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "custom",
-  },
 };
 
 module.exports = nextConfig;
-// next.config.js
